0134b04b7cbe7dc14a10c2b6f52ea1130a7c97a5
destino92 <destinodello@gmail.com>
2017-05-13 02:14:39 +0300

    feat: added Gallery compnent to display tracks with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
-import Profile from './Profile'
+import Profile from './Profile';
+import Gallery from './Gallery';
 
 class App extends Component {
   constructor(props){
@@ -69,9 +70,9 @@ class App extends Component {
               <Profile
                 artist={this.state.artist}
               />
-              <div className="Gallery">
-                Gallery
-              </div>
+              <Gallery
+                tracks={this.state.tracks}
+              />
             </div>
           : <div></div>
         }
